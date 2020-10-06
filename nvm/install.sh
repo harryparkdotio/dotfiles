a9e6801ab427b2e7991d0439edac38a70f62b416
Harry Park <harry@harrypark.io>
2020-10-06 22:12:28 +1100

    fix: nvm install

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+if [[ ! -f "$NVM_DIR/nvm.sh" ]]; then
+  echo "  Installing nvm for you."
+
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+fi
