f5276463b14597f32b4cd3c8c458bac628a6d7d8
Harry Park <harry@harrypark.io>
2020-10-06 21:42:30 +1100

    fix: homebrew install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -7,24 +7,30 @@ if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install coreutils
-brew install git
-brew install gnupg
-brew install golang
-brew install grep
-brew install kubectl
-brew install kubectx
-brew install vim --with-override-system-vi
-brew install wget --with-iri
-brew install zsh
+brew_add() { if brew ls --versions "$1"; then HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade "$1"; else HOMEBREW_NO_AUTO_UPDATE=1 brew install "$1"; fi }
 
-brew tap caskroom/cask
+brew_add coreutils
+brew_add git
+brew_add gnupg
+brew_add golang
+brew_add grep
+brew_add kubectl
+brew_add kubectx
+brew_add vim
+brew_add wget
+brew_add zsh
 
-brew cask install github
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install spotify
-brew cask install visual-studio-code
+brew tap homebrew/cask
+
+CASKS=(
+  github
+  google-chrome
+  iterm2
+  spotify
+  visual-studio-code
+)
+
+brew cask install ${CASKS[@]}
 
 brew cleanup
 
