2edfdabda6984160169572d9cd3a207f4981648a
Harry Park <harry@harrypark.io>
2020-10-11 16:08:29 +1100

    perf: remove ohmyzsh

--- oh-my-zsh/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-ZSH_CUSTOM="$ZSH/custom"
-
-curl https://raw.githubusercontent.com/tylerreckart/hyperzsh/master/hyperzsh.zsh-theme > $ZSH_CUSTOM/themes/hyperzsh.zsh-theme
